c7140a497a0bea849006e52f4586b9a476d48ad6
Jordi Lazo <jordilazo94@gmail.com>
2021-11-29 14:33:17 +0100

    App

---
+++ app/app/src/main/java/com/example/myapplication/ReadData.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class ReadData extends AppCompatActivity {
+
+    DatabaseReference databaseReference;
+    Firebase
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
