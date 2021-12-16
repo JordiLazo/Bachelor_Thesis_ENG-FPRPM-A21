9b7a3134a362f4cc200dbc7706a66a3991750975
Jordi Lazo <jordilazo94@gmail.com>
2021-12-16 19:55:59 +0100

    Update CheckRoomFragment.java

--- app/app/src/main/java/com/example/myapplication/schedulemanager/ui/checkroom/CheckRoomFragment.java
+++ app/app/src/main/java/com/example/myapplication/schedulemanager/ui/checkroom/CheckRoomFragment.java
@@ -57,7 +57,7 @@ public class CheckRoomFragment extends Fragment {
         });
 
         return root;
-}
+    }
 
     private void loadPieChartData(String item) {
         dataBase.child("classroom").child(item).addListenerForSingleValueEvent(new ValueEventListener() {
